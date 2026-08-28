6ba402e77181d7449cc7d540f8822e138018f4d3
jash_nikumbhe <jashunik9@gmail.com>
2026-08-28 13:56:13 +0530

    implement main engine with topological sort for backward pass

---
+++ engine.py
@@ -0,0 +1,72 @@
+class Value:
+    def __init__(self, data, _children=(), _op=''):
+        self.data = float(data)
+        self.prev = set(_children)
+        self._op = _op
+        self._backward = lambda: None
+        self.grad = 0.0
+    
+    def __repr__(self):
+        return f"Value (data = {self.data})"
+
+    def __add__(self, other):
+        sum = self.data + other.data
+        children = (self, other)
+        output = Value(sum, children, '+')
+
+        def _backward():
+            self.grad += output.grad
+            other.grad += output.grad
+
+        output._backward = _backward
+        return output        
+        
+    def __sub__(self, other):
+        difference = self.data - other.data
+        children = (self, other)
+        output = Value(difference, children, '-')
+
+        def _backward():
+            self.grad += output.grad
+            other.grad += -output.grad
+
+        output._backward = _backward
+        return output        
+        
+    def __mul__(self, other):
+        product = self.data * other.data
+        children = (self, other)
+        output = Value(product, children, '*')
+        
+        def _backward():
+            self.grad += other.data * output.grad
+            other.grad += self.data * output.grad
+
+        output._backward = _backward
+        return output
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v.prev:
+                    build_topo(child)
+                topo.append(v)
+        
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+if __name__ == "__main__":
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a * b
+    d = c + b
+    d.backward()
+    print(f"d.grad: {d.grad}") 
+    print(f"a.grad: {a.grad}")
+    print(f"b.grad: {b.grad}") 
